86b1fdd26fe38a6e56d7cc43c380678b7694c0d3
vikash285 <vikashdhaka285@gmail.com>
2023-07-02 15:31:36 +0530

    bootstrap css, edit, pagination twiks

--- public/Login/login.js
+++ public/Login/login.js
@@ -1,23 +1,33 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  if (!form.checkValidity()) {
+    event.preventDefault();
+  }
+  form.classList.add("was-validated");
+});
+
 async function logIn(event) {
-    try {
+  try {
+    event.preventDefault(event);
 
-        event.preventDefault(event)
-        
-        const loginDetails = {
-            email: event.target.email.value,
-            password: event.target.password.value
-        }
+    const loginDetails = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
 
-        const res = await axios.post("http://localhost:3000/userApp/login", loginDetails)
-        
-            alert(res.data.message)
-            localStorage.setItem('token', res.data.token)
-            window.location.href = "../ExpenseTracker/index.html"
-    } catch (err) {
-        document.body.innerHTML += `<div style="color:red;">${err.message}</div>`
-    }
+    const res = await axios.post(
+      "http://localhost:3000/userApp/login",
+      loginDetails
+    );
+
+    alert(res.data.message);
+    localStorage.setItem("token", res.data.token);
+    window.location.href = "../ExpenseTracker/index.html";
+  } catch (err) {
+    document.body.innerHTML += `<div style="color:red;">${err.message}</div>`;
+  }
 }
 
-function forgotPassword () {
-    window.location.href = "../ForgotPassword/index.html"
+function forgotPassword() {
+  window.location.href = "../ForgotPassword/index.html";
 }
